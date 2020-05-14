b54249a181014fdebd8df8ec120a9def59426a5a
Shubhashis Roy <iamdipta@gmail.com>
2020-05-14 15:31:38 +0600

    added readme for index of files

--- Data-Structures/Trie.py
+++ Data-Structures/Trie.py
@@ -1,5 +1,5 @@
 class Pointer:
-    
+
     def __init__(self):
         self.a = [None for _ in range(30)]
         self.endPoint = False
@@ -8,16 +8,9 @@ class Pointer:
 class Trie:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.head = Pointer()
-        
 
     def insert(self, word: str) -> None:
-        """
-        Inserts a word into the trie.
-        """
         curr = self.head
         for v in word:
             nw = ord(v) - 97
@@ -25,12 +18,8 @@ class Trie:
                 curr.a[nw] = Pointer()
             curr = curr.a[nw]
         curr.endPoint = True
-        
 
     def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the trie.
-        """
         curr = self.head
         for v in word:
             nw = ord(v) - 97
@@ -38,12 +27,8 @@ class Trie:
                 return False
             curr = curr.a[nw]
         return curr.endPoint
-    
 
     def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
         curr = self.head
         for v in prefix:
             nw = ord(v) - 97
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cookbook-python
 Cookbook for Competitive Programming in Python
+
+## [Template](./template.py)
+## Data Structures
+1. [Trie](./Data-Structures/trie.py)
