e925d75ce1a013129f3078850f29a6e001727918
Shubhashis Roy <iamdipta@gmail.com>
2020-05-14 16:19:42 +0600

    added ds

---
+++ data-structures/trie.py
@@ -0,0 +1,38 @@
+class Pointer:	
+
+    def __init__(self):	
+        self.a = [None for _ in range(30)]	
+        self.endPoint = False	
+
+
+class Trie:	
+
+    def __init__(self):	
+        self.head = Pointer()	
+
+    def insert(self, word: str) -> None:	
+        curr = self.head	
+        for v in word:	
+            nw = ord(v) - 97	
+            if curr.a[nw] is None:	
+                curr.a[nw] = Pointer()	
+            curr = curr.a[nw]	
+        curr.endPoint = True	
+
+    def search(self, word: str) -> bool:	
+        curr = self.head	
+        for v in word:	
+            nw = ord(v) - 97	
+            if curr.a[nw] is None:	
+                return False	
+            curr = curr.a[nw]	
+        return curr.endPoint	
+
+    def startsWith(self, prefix: str) -> bool:	
+        curr = self.head	
+        for v in prefix:	
+            nw = ord(v) - 97	
+            if curr.a[nw] is None:	
+                return False	
+            curr = curr.a[nw]	
+        return True 
